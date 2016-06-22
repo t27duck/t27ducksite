d6859bdaa6ca341d9ee41d237251f5bf55578c04
Tony Drake <t27duck@gmail.com>
2016-06-22 19:45:39 -0400

    Cleaned up auth class

--- app/models/stupidly_simple_authentication.rb
+++ app/models/stupidly_simple_authentication.rb
@@ -1,8 +1,6 @@
 require 'bcrypt'
 
 class StupidlySimpleAuthentication
-  @config = {}
-
   def self.authenticate(password)
     BCrypt::Password.new(password_digest) == password
   end
@@ -11,16 +9,22 @@ class StupidlySimpleAuthentication
     session_token == token
   end
 
+  def self.session_token
+    config['session_token'] || raise('Session token not set')
+  end
+
+  def self.config
+    @config ||= configure!
+  end
+  private_class_method :config
+
+  def self.configure!
+    @config = YAML::load_file("#{Rails.root}/.authentication.yml")
+  end
+  private_class_method :configure!
+
   def self.password_digest
-    @config['password_digest'] || raise('Digest not set')
+    config['password_digest'] || raise('Digest not set')
   end
   private_class_method :password_digest
-
-  def self.session_token
-    @config['session_token'] || raise('Session token not set')
-  end
-
-  def self.configure!(path="#{Rails.root}/.authentication.yml")
-    @config = YAML::load_file(path)
-  end
 end
--- config/initializers/stupidly_simple_authentication.rb
+++
@@ -1,1 +0,0 @@
-StupidlySimpleAuthentication.configure!
