b026816d0c1d44e7069d383e6d5ff82f9b6ca77f
Tony Drake <t27duck@gmail.com>
2016-06-24 16:17:46 -0400

    Misc cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,5 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: [:show]
-
   def show
-  end
-
-  private ######################################################################
-
-  def set_page
     @page = Page.find_by!(slug: params[:id].downcase)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
+  MARKDOWN_OPTIONS = {
+    fenced_code_blocks: true,
+    no_intra_emphasis:  true,
+    autolink:           true,
+    lax_html_blocks:    true
+  }.freeze
+
   def markdown(template)
     renderer = CodeRayify.new(filter_html: true, hard_wrap: true)
 
-    options = {
-      fenced_code_blocks: true,
-      no_intra_emphasis:  true,
-      autolink:           true,
-      lax_html_blocks:    true
-    }
-
-    Redcarpet::Markdown.new(renderer, options).render(template).html_safe
+    Redcarpet::Markdown.new(renderer, MARKDOWN_OPTIONS).render(template).html_safe
   end
 end
