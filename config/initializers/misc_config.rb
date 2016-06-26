2aea9f0823db3f1b0434eec58527167a3e91ddd2
Tony Drake <t27duck@gmail.com>
2016-06-26 18:02:23 -0400

    Rubocop fixes

--- config/initializers/misc_config.rb
+++ config/initializers/misc_config.rb
@@ -1,1 +1,5 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/misc.yml").freeze rescue {}.freeze
+CONFIG = begin
+           YAML.load_file("#{Rails.root}/config/misc.yml").freeze
+         rescue
+           {}.freeze
+         end
