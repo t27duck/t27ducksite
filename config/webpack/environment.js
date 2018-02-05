2102616a3bac62235443a13037d2d3193ca14d46
Tony Drake <t27duck@gmail.com>
2018-02-05 18:13:15 -0500

    Add expose-loader to make jquery globally available

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,22 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-const webpack = require('webpack')
+const webpack = require('webpack');
 environment.plugins.set('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery'
-}))
+}));
 
-module.exports = environment
+const jqueryLoader = {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: 'jQuery'
+  }, {
+    loader: 'expose-loader',
+    options: '$'
+  }]
+};
+
+environment.loaders.append('expose-loader-jquery', jqueryLoader);
+
+module.exports = environment;
