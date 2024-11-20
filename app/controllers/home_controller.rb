cbb0308f4b3b573fb2cdae94848eaeb8b00dffe9
Tony Drake <t27duck@gmail.com>
2024-11-20 23:59:07 +0000

    Increase homepage lists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 
 class HomeController < ApplicationController
   def index
-    @posts = Post.published.order(published_at: :desc).limit(5)
-    @talks = Talk.order(presented_on: :desc).limit(5)
+    @posts = Post.published.order(published_at: :desc).limit(8)
+    @talks = Talk.order(presented_on: :desc).limit(8)
   end
 end
