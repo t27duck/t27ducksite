7f2594483d38d058303703f9a0842ea77de8e705
Tony Drake <t27duck@gmail.com>
2025-08-02 09:10:35 -0400

    Update importmap integrity and packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+enable_integrity!
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", integrity: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", integrity: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", integrity: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", integrity: true
-pin_all_from "app/javascript/controllers", under: "controllers", integrity: true
-pin "@avo-hq/marksmith", to: "@avo-hq--marksmith.js", integrity: true # @0.2.1
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@avo-hq/marksmith", to: "@avo-hq--marksmith.js" # @0.4.5
